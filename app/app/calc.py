eb5191596544ba02199bb370e80aed85549786de
Gonzalo Fernández <gonzalofn95@gmail.com>
2025-09-16 09:05:52 +0200

    Update

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Adds x and y and returns the result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtracts x and y and gives the result"""
+    return y - x
