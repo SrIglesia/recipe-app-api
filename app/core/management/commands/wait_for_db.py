a345961c7d3bef5c5794b40c88e38b18dd7555d6
Gonzalo Fernández <gonzalofn95@gmail.com>
2025-11-10 17:54:28 +0100

    Added health api

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,14 +4,14 @@ Django command to wait for the database to be available
 
 import time
 
-from psycopg2 import OperationalError as Pyscopg2OpError
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database"""
-    
+
     def handle(self, *args, **options):
         """Entrypoint for command"""
         self.stdout.write('Waiting for database...')
@@ -20,7 +20,7 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Pyscopg2OpError, OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
